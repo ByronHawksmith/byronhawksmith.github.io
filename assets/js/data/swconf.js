5941012624fc218d68226fd79e21b7abd8f498f0
ByronHawksmith <ByronHawksmith@users.noreply.github.com>
2025-01-12 07:30:10 +0000

    deploy: 8fa28eeff71de5feb99b36387b3c4f633abeeb14

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736666005',resources: [
+    cacheName: 'chirpy-1736667010',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
